bd77b0f25935ba1fe8380638c2725c62061899eb
Arthur Corenzan <arthur@corenzan.com>
2013-01-09 19:48:27 -0200

    ...

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var express, app, pg;
+var express, app, pg, db;
 
 express = require('express');
 pg = require('pg');
+
+db = new pg.Client(process.env.DATABASE_URL);
+db.connect();
 
 app = express();
 app.use(express.logger());
@@ -11,15 +14,8 @@ app.get('/', function(request, response) {
 
   q = 'SELECT LG."CEP", LB."BAI_NO", LC."LOC_NO", LG."UFE_SG", LG."TLO_TX", LG."LOG_NO" FROM "LOG_LOGRADOURO" LG LEFT JOIN "LOG_LOCALIDADE" LC ON LC."LOC_NU" = LG."LOC_NU" LEFT JOIN "LOG_BAIRRO" LB ON LG."BAI_NU_INI" = LB."BAI_NU" WHERE LG."CEP" = $1 LIMIT 1;';
 
-  pg.connect(process.env.DATABASE_URL, function(error, db) {
-    if(error) {
-      response.json(error);
-    } else {
-      db.query(q, [request.query.CEP], function(error, result) {
-        response.json(error || result.rows);
-        db.end();
-      });
-    }
+  db.query(q, [request.query.CEP], function(error, result) {
+    response.json(error || result.rows);
   });
 });
 
